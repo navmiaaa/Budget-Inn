ef53bbf1d7fb4a9e13ef7bb44f3c28996aa2ca4e
navmiaaa <navmia2116@gmail.com>
2026-07-26 00:58:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.jsx';   // ← .jsx hona chahida
 import './App.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
