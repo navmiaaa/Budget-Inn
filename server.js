2ead8d529319a3ca8c7c60589479312d2da28e5c
navmiaaa <navmia2116@gmail.com>
2026-07-31 16:38:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -35,3 +35,4 @@ app.post('/api/bookings', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Budget Inn API running on port ${PORT}`));
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
